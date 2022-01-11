0f5c275c0e303c07a8329d575ff17e551dac992e
Brian Parker <btparker70@gmail.com>
2022-01-10 21:22:01 -0800

    refactored filter. added negatives filter

--- index.js
+++ index.js
@@ -11,12 +11,24 @@ const stringCalculator = (input) => {
   
   function add(str, delimiter = ',') {
     let subStrings = str.split(delimiter).map( Number );
-    let sum = subStrings.filter(num => num < 1000).reduce((a, b) => a + b);
+
+    let negativesCounter = [];
+    subStrings = subStrings.filter(function(item) {
+    if (item >= 1000) return false;
+    if (Math.sign(item) === -1) {
+      negativesCounter.push(item);
+    }
+    return true
+    })
+    if (negativesCounter.length) throw `Negatives not allowed at ${negativesCounter.toString()}`
+
+    let sum = subStrings.reduce((a, b) => a + b);
     console.log(sum)
     return sum;
   }
+
 }
 
 // stringCalculator("//;\n1;3;4")
-stringCalculator("1,2,3,4,999")
+stringCalculator("1,-2,-3,4,1000,-3,-2")
 // stringCalculator("//@@@@@\n1@@@@@3@@@@@4")
